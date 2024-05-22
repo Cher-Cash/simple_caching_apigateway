6572e82e51f205668e7d8f362c5b06c425dd07e9
leha <kovach@gmail.com>
2024-05-22 20:54:21 +1000

    add retry

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import requests
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from flask_caching import Cache
-
-
-
+from time import sleep
 
 app = Flask(__name__)
 
-
 app.config['CACHE_TYPE'] = 'SimpleCache'  # Можно выбрать другой тип кэша, например, Redis
 cache = Cache(app)
+
 
 def get_operator_name(number):
     print('get operator name ', number)
@@ -28,30 +26,40 @@ def get_operator_name(number):
     return response.json()
 
 
-def cached_call(number=None):
+def cached_call(number=None, retry=3, timeout=2):
     cached = cache.get(number)
+    if cached is not None:
+        return cached
 
-    if cached is None:
+    for i in range(0, retry):
         json_from_voxlink = get_operator_name(number)
-        if json_from_voxlink != None:
+        if json_from_voxlink is not None:
             cache.set(number, json_from_voxlink)
-        return json_from_voxlink
+            cached = json_from_voxlink
+        else:
+            sleep(timeout)
     return cached
 
 
 @app.route('/voxlink')
 def voxlink_json():
     number = request.args.get('num', None)
-    return cached_call(number)
+    retry = app.config.get('retry', 3)
+    timeout = app.config.get('timeout', 2)
+    return jsonify(cached_call(number, retry=retry, timeout=timeout))
+
 
 @app.route('/voxlink/operator')
 def voxlink_operator():
     number = request.args.get('num', None)
-    json_from_voxlink = cached_call(number)
+    retry = app.config.get('retry', 3)
+    timeout = app.config.get('timeout', 2)
+    json_from_voxlink = cached_call(number, retry=retry, timeout=timeout)
     if json_from_voxlink is not None:
         operator_name = json_from_voxlink.get('operator')
         return operator_name
     return ''
+
 
 if __name__ == "__main__":
     number = 79625864175
